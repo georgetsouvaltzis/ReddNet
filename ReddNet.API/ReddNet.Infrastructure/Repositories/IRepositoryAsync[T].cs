e709e4d21f7413908a2d0482aa097ed37d2dfaef
George Tsouvaltzis <tsouvaltzis.george@gmail.com>
2022-10-29 15:18:30 +0400

    Added IAsyncRepositoryTemplate

---
+++ ReddNet.API/ReddNet.Infrastructure/Repositories/IRepositoryAsync[T].cs
@@ -0,0 +1,13 @@
+﻿using ReddNet.Domain;
+
+namespace ReddNet.Infrastructure.Repositories;
+
+
+public interface IRepositoryAsync<TEntity> where TEntity: BaseEntity
+{
+    Task GetByIdAsync();
+    Task GetAllAsync();
+    Task AddAsync();
+    Task DeleteAsync();
+    Task UpdateAsync();
+}
